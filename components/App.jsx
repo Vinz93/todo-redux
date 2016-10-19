2ec8451f2de517ba412ed02fd93da7758bbda6d7
Vincenzo Bianco <vincenzob@ludopia.net>
2016-10-19 15:14:06 -0400

    separate App and Footer

---
+++ components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import VisibleTodoList from '../containers/VisibleTodoList';
+import AddTodo from '../containers/AddTodo';
+import Footer from './Footer';
+
+const TodoApp = () => {
+  return(
+    <div>
+      <AddTodo />
+      <VisibleTodoList  />
+      <Footer  />
+    </div>
+  );
+}
+
+export default TodoApp
---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import FilterLink from '../containers/FilterLink';
+
+const Footer = () => {
+  return(
+    <div>
+      Show:
+      {' '}
+      <FilterLink filter='SHOW_ALL'>
+        All
+      </FilterLink>
+      {' '}
+      <FilterLink filter='SHOW_ACTIVE'>
+        Active
+      </FilterLink>
+      {' '}
+      <FilterLink filter='SHOW_COMPLETED'>
+        Completed
+      </FilterLink>
+    </div>
+  );
+}
+
+export default Footer
