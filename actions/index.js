398a24e837ffca87bf52e5d7bb58931eb9981805
Vincenzo Bianco <vincenzo.bianco@wikot.com>
2016-11-05 15:53:18 -0400

    Use Link from react-router

--- actions/index.js
+++ actions/index.js
@@ -6,13 +6,6 @@ export const addTodo = (text) => ({
      id: v4()
    });
 
-
-export const setVisibilityFilter = (filter) => ({
-     type: 'SET_VISIBILITY_FILTER',
-     filter
-  });
-
-
 export const toggleTodo = (id) => ({
     type: 'TOGGLE_TODO',
     id
--- containers/FilterLink.jsx
+++ containers/FilterLink.jsx
@@ -1,16 +1,16 @@
-import { connect } from 'react-redux';
-import Link from '../components/Link';
-import { setVisibilityFilter } from '../actions';
+import React from 'react';
+import { Link } from 'react-router';
 
-const mapStateToProps = (state, ownProps) => ({
-    active: ownProps.filter === state.visibilityFilter
-  });
+const FilterLink = ({ filter, children }) => (
+  <Link
+    to={filter === 'all' ? '' : filter}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black',
+    }}
+    >
+    {children}
+  </Link>
+);
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
-  });
-
-
-const FilterLink = connect(mapStateToProps, mapDispatchToProps)(Link);
-
-export default FilterLink
+export default FilterLink;
