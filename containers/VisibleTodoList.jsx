6ae96e393486dc4bc3e4e6ed013802afae29c85f
Vincenzo Bianco <vincenzo.bianco@wikot.com>
2016-11-09 07:56:56 -0400

    move getVisibleTodos to reducers as selector

--- containers/VisibleTodoList.jsx
+++ containers/VisibleTodoList.jsx
@@ -2,33 +2,24 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import TodoList from '../components/TodoList';
 import { toggleTodo } from '../actions';
+import { getVisibleTodos } from '../reducers/todos';
 
- const getVisibleTodos = (todos, filter) => {
-   switch(filter){
-     case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter( t => t.completed );
-    case 'active':
-      return todos.filter( t => !t.completed );
-   }
- }
 
  const mapStoreToProps = (state, { params}) => ({
      todos: getVisibleTodos(state.todos, params.filter || 'all')
    });
 
 
- const mapDispatchToProps = dispatch => ({
-     onTodoClick(id) {
-       dispatch(toggleTodo(id));
-     }
-   });
+ // const mapDispatchToProps = dispatch => ({
+ //     onTodoClick(id) {
+ //       dispatch(toggleTodo(id));
+ //     }
+ //   });
 
 
 const VisibleTodoList = withRouter(connect(
   mapStoreToProps,
-  mapDispatchToProps
+  { onTodoClick: toggleTodo }
 )(TodoList));
 
 export default VisibleTodoList
--- reducers/todos.js
+++ reducers/todos.js
@@ -34,3 +34,14 @@ const todos = (state = [], action) =>{
 };
 
 export default todos
+
+export const getVisibleTodos = (state, filter) => {
+  switch(filter){
+    case 'all':
+     return state;
+   case 'completed':
+     return state.filter( t => t.completed );
+   case 'active':
+     return state.filter( t => !t.completed );
+  }
+};
